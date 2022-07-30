cba9f6e3a6b251af762566dee477ccbf70c266c4
ropodl <sarox14@gmail.com>
2022-07-30 11:12:28 +0545

    disqus

--- resources/views/frontend/news_detail.blade.php
+++ resources/views/frontend/news_detail.blade.php
@@ -46,7 +46,7 @@
                                     @endif
                                     {!! $paragraph !!}</p>
                                 @endforeach
-                                <div class="sharethis-inline-reaction-buttons"></div>
+                                <!-- <div class="sharethis-inline-reaction-buttons"></div> -->
                             </div>
                         </div>
                     </div>
@@ -133,6 +133,29 @@
                                 @endif
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                    <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://https-rajdhanipress-com-1.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
                     </div>
                 </div>
                 <div class="rajniti pt-3 pb-3">
