5c5bc197cfde3bf486b3e61434030dbf1f9558a6
ropodl <sarox14@gmail.com>
2022-08-06 16:57:51 +0545

    margin between content and paragraph indetail page

--- resources/views/frontend/advertisement/l_ad.blade.php
+++ resources/views/frontend/advertisement/l_ad.blade.php
@@ -1,3 +1,3 @@
 <a href='{{$url}}' target='_blank' class='ads-large'>
-    <img src='{{ $image }}' alt='' class='sc-logo__img w-100'>
+    <img src='{{ $image }}' alt='' class='sc-logo__img w-100 mb-4'>
 </a>
