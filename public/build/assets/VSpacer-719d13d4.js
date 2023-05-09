ea600b17835b754d1734e56e6bb59b2a93d79ede
ropodl <sarox14@gmail.com>
2023-05-09 13:29:07 +0545

    new build test

---
+++ public/build/assets/VSpacer-719d13d4.js
@@ -0,0 +1,1 @@
+import{c as e}from"./createSimpleFunctional-0dce9a2b.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- resources/js/pages/index.vue
+++ resources/js/pages/index.vue
@@ -75,6 +75,7 @@ const twoFour = defineAsyncComponent(() =>
         </v-row>
     </v-container>
     <template v-for="(item, index) in catnews">
+        {{ item }}
         <template v-if="item['name'] == 'राजनीति' && item['news'].length > 0">
             <v-container>
                 <overlayFull :data="item"></overlayFull>
