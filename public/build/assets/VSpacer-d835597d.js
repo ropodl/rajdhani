409c2d09bb1beec0ad57830f273ff84ca6968604
ropodl <sarox14@gmail.com>
2023-05-09 13:00:40 +0545

    build update

--- public/build/assets/VSpacer-d835597d.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createSimpleFunctional-0ce1e943.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,8 +1,8 @@
 // Plugins
-import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
+// import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import { loadFontAwesome } from "./fontawesome";
-import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
+import { loadFonts } from "./webfontloader";
 
 export function registerPlugins(app) {
     loadFonts();
