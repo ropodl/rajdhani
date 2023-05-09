2cd464d78928b6063aa93311048713c8bf9ef7b8
ropodl <sarox14@gmail.com>
2023-05-09 21:12:54 +0545

    new build with new features

--- public/build/assets/VSpacer-ceee00d3.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createSimpleFunctional-a40a1395.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -6,7 +6,7 @@ import { loadFonts } from "./webfontloader";
 
 export function registerPlugins(app) {
     loadFonts();
-    // loadFontAwesome(app);
-    // app.use(vuetify);
+    loadFontAwesome(app);
+    app.use(vuetify);
     // .use(autoAnimatePlugin);
 }
